7e47af1e673e2c4c14e1040a2013fca5af59c2aa
Aztec Eagle Turbo <turbo@gendo-ikari.ier.unam.mx>
2021-06-18 18:10:42 -0500

    First sketch, Gravitiy CO2 Sensor, dfrobot sen0219 Program sample modified to use Serial Plotter

---
+++ GravityCO2/GravityCO2.ino
@@ -0,0 +1,56 @@
+/***************************************************
+ * Infrared CO2 Sensor0-5000ppm
+ * ****************************************************
+ * This example The sensors detect CO2
+ *
+ * @author lg.gang(lg.gang@qq.com)
+ * @version  V1.0
+ * @date  2016-7-6
+ *
+ * GNU Lesser General Public License.
+ * See <http://www.gnu.org/licenses/> for details.
+ * All above must be included in any redistribution
+ *
+ * Modified by <hdcg@ier.unam.mx> to use SerialPlotter
+ *
+ * ****************************************************/
+int sensorIn = A0;
+
+void setup(){
+  Serial.begin(38400);
+  // Set the default voltage of the reference voltage
+  analogReference(DEFAULT);
+  Serial.println("sensorValue voltage co2ppm");
+}
+
+void loop(){
+  //Read voltage
+  int sensorValue = analogRead(sensorIn);
+
+  // The analog signal is converted to a voltage
+  float voltage = sensorValue*(5000/1024.0);
+  if(voltage == 0)
+  {
+    Serial.println("Fault");
+  }
+  else if(voltage < 400)
+  {
+    Serial.println("preheating");
+  }
+  else
+  {
+    int voltage_diference=voltage-400;
+    float concentration=voltage_diference*50.0/16.0;
+    // Print sensorValue
+    Serial.print(sensorValue);
+    // Print Voltage
+    Serial.print(" ");
+    Serial.print(voltage);
+    // Serial.println("mv");
+    //Print CO2 concentration
+    Serial.print(" ");
+    Serial.println(concentration);
+    //Serial.println("ppm");
+  }
+  delay(1024);
+}
